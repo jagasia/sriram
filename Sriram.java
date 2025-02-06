ab2effdd92f6d41a20c192e47fb6bb14e0176957
jagasia <rjagadeeswaran@Yahoo.com>
2025-02-06 09:29:50 +0530

    06Feb 09:29

--- Sriram.java
+++ Sriram.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class Sriram
 {
 	public static void main(String args[])		
 	{	
+		//This is sriram, who is typing the following:
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Hello world");
 	}
 }
